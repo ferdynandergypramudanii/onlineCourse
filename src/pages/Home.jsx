e7c32f94d5761e874caaba21cd4950dfe6395309
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-10-12 07:47:33 +0700

    add course-section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import "../styles/Home.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { homeSection } from "../data/HomeSection";
+import { courseSection } from "../data/CourseSection";
 import parser from "html-react-parser";
 
 function Home() {
@@ -13,6 +14,11 @@ function Home() {
         <section id="home">
           <img src={homeSection.image} />
           <div className="kolom">{parser(homeSection.content)}</div>
+        </section>
+
+        <section id="courses">
+          <div class="kolom">{parser(courseSection.content)}</div>
+          <img src={courseSection.image} />
         </section>
       </div>
 
